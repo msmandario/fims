8430ae3ad053da8b4dd2e7fb0e58c027d90de096
Eriene Galinato <edgalinato@up.edu.ph>
2026-01-31 02:02:59 +0800

    deps(`playwright`): configure test file name pattern matching

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     webServer: { command: 'npm run build && npm run preview', port: 4173 },
     testDir: 'tests',
+    testMatch: /.*e2e.(js|ts)/,
 });
