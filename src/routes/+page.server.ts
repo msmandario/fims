e76bb165f761342a57f6a527fd4f2746d07db8ac
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-12 11:12:40 +0800

    feat: use dummy faculty record list data

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,17 @@
 import { getFacultyRecordList } from "$lib/server/db-helpers";
 
 export async function load() {
-    const facultyRecordList = await getFacultyRecordList();
+    // const facultyRecordList = await getFacultyRecordList();
+    const facultyRecordList = [
+        {
+            facultyid: 1,
+            lastname: 'Dela Cruz',
+            firstname: 'Juan',
+            status: 'Active',
+            ranktitle: 'Professor 7',
+            adminposition: 'Department Chair',
+        }
+    ]
+
     return { facultyRecordList }
 }
