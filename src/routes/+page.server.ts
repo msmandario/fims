78a67883f92d1ef3e86d4fe0930e4d6998e1d907
Eriene Galinato <edgalinato@up.edu.ph>
2026-02-12 11:12:16 +0800

    feat: load faculty record list

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,6 @@
-// src/routes/dashboard/+page.server.ts
-import { db } from '$lib/server/db/index';
-import { faculty } from '$lib/server/db/schema';
+import { getFacultyRecordList } from "$lib/server/db-helpers";
 
 export async function load() {
-    // Task 4: Fetch faculty list from DB
-    try {
-        const facultyList = await db.select().from(faculty);
-        return {
-            facultyList,
-        };
-    } catch (e) {
-        console.error('DB Fetch Error:', e);
-        return { facultyList: [], error: 'Database connection failed.' };
-    }
+    const facultyRecordList = await getFacultyRecordList();
+    return { facultyRecordList }
 }
